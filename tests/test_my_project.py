f7fe4e2ab12dc1eb4b2b26ea1a4e7da58b4a1182
limkaleb <limkaleb@gmail.com>
2021-07-05 23:16:17 +0700

    first commit

---
+++ tests/test_my_project.py
@@ -0,0 +1,11 @@
+from my_project import __version__
+from my_project.math import multiply_two_numbers
+
+
+def test_multiply_two_numbers():
+    result = multiply_two_numbers(2, 3)
+    assert result == 6
+
+
+def test_version():
+    assert __version__ == "0.1.0"
